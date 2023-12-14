b180e9b5936a05b1f7e849ddaa5039a2ca1fff18
yousef <yousefkamel282@gmail.com>
2023-12-14 19:19:37 +0200

    line follower code edited!!

--- Code/code1.ino
+++ Code/code1.ino
@@ -1,98 +1,97 @@
 #define IR_SENSOR_RIGHT 11
 #define IR_SENSOR_LEFT 12
-#define MOTOR_SPEED 180
 
 //Right motor
-#define enableRightMotor 6
-#define rightMotorPin1 7
-#define rightMotorPin2 8
+#define ENA 6
+#define IN1 7
+#define IN2 8
 
 //Left motor
-#define enableLeftMotor 5
-#define leftMotorPin1 9
-#define leftMotorPin2 10
+#define ENB 5
+#define IN3 9
+#define IN4 10
 
-void setup()
-{
-  pinMode(enableRightMotor, OUTPUT);
-  pinMode(rightMotorPin1, OUTPUT);
-  pinMode(rightMotorPin2, OUTPUT);
-   
-  pinMode(enableLeftMotor, OUTPUT);
-  pinMode(leftMotorPin1, OUTPUT);
-  pinMode(leftMotorPin2, OUTPUT);
+void setup() {
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
 
-  pinMode(IR_SENSOR_RIGHT, INPUT);
   pinMode(IR_SENSOR_LEFT, INPUT);
-  rotateMotor(0,0);   
+  pinMode(IR_SENSOR_RIGHT, INPUT);
+
+  Serial.begin(9600);
 }
 
+void loop() {
+  int leftSensorValue = digitalRead(IR_SENSOR_LEFT);
+  int rightSensorValue = digitalRead(IR_SENSOR_RIGHT);
 
-void loop()
-{
+  Serial.print("Left Sensor: ");
+  Serial.print(leftSensorValue);
+  Serial.print("  Right Sensor: ");
+  Serial.println(rightSensorValue);
 
-  int rightIRSensorValue = digitalRead(IR_SENSOR_RIGHT);
-  int leftIRSensorValue = digitalRead(IR_SENSOR_LEFT);
-
-  //If none of the sensors detects black line, then go straight
-  if (rightIRSensorValue == LOW && leftIRSensorValue == LOW)
-  {
-    rotateMotor(MOTOR_SPEED, MOTOR_SPEED);
-  }
-  //If right sensor detects black line, then turn right
-  else if (rightIRSensorValue == HIGH && leftIRSensorValue == LOW )
-  {
-      rotateMotor(-MOTOR_SPEED, MOTOR_SPEED); 
-  }
-  //If left sensor detects black line, then turn left  
-  else if (rightIRSensorValue == LOW && leftIRSensorValue == HIGH )
-  {
-      rotateMotor(MOTOR_SPEED, -MOTOR_SPEED); 
-  } 
-  //If both the sensors detect black line, then stop 
-  else 
-  {
-    rotateMotor(0, 0);
+  if (leftSensorValue == LOW && rightSensorValue == LOW) {
+    // Both sensors on the line - move forward
+    moveForward();
+  } else if (leftSensorValue == HIGH && rightSensorValue == LOW) {
+    // Left sensor off the line - turn left
+    turnLeft();
+  } else if (leftSensorValue == LOW && rightSensorValue == HIGH) {
+    // Right sensor off the line - turn right
+    turnRight();
+  } else {
+    // Lost the line - stop
+    stopMotors();
   }
 }
 
+void moveForward() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, 80); 
 
-void rotateMotor(int rightMotorSpeed, int leftMotorSpeed)
-{
-  
-  if (rightMotorSpeed < 0)
-  { 
-    // backward
-    digitalWrite(rightMotorPin1,LOW);
-    digitalWrite(rightMotorPin2,HIGH);    
-  }
-  else if (rightMotorSpeed > 0)
-  {
-    // forward
-    digitalWrite(rightMotorPin1,HIGH);
-    digitalWrite(rightMotorPin2,LOW);      
-  }
-  else
-  { // stop
-    digitalWrite(rightMotorPin1,LOW);
-    digitalWrite(rightMotorPin2,LOW);      
-  }
-
-  if (leftMotorSpeed < 0)
-  {
-    digitalWrite(leftMotorPin1,LOW);
-    digitalWrite(leftMotorPin2,HIGH);    
-  }
-  else if (leftMotorSpeed > 0)
-  {
-    digitalWrite(leftMotorPin1,HIGH);
-    digitalWrite(leftMotorPin2,LOW);      
-  }
-  else 
-  {
-    digitalWrite(leftMotorPin1,LOW);
-    digitalWrite(leftMotorPin2,LOW);      
-  }
-  analogWrite(enableRightMotor, abs(rightMotorSpeed));
-  analogWrite(enableLeftMotor, abs(leftMotorSpeed));    
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENB, 80); 
 }
+
+void turnLeft() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  analogWrite(ENA, 20); 
+
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENB, 60); 
+}
+
+void turnRight() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, 60); 
+
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(ENB, 20); 
+}
+
+void stopMotors() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, 0);
+
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENB, 0);
+}
+
+
+
+
+
+
+
