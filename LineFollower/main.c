f335da1edc63db98bdff1fb8b7270af3c063d607
Ahmed Elnozahy <108683164+AhmedSamymoh@users.noreply.github.com>
2023-12-25 03:52:54 +0200

    All is Done

--- LineFollower/main.c
+++ LineFollower/main.c
@@ -1,17 +1,20 @@
 #include <REG51.H>
 //#include <AT89X51.H>
 
+typedef unsigned short int   uint8_t;
 
+uint8_t period, ON_Period, OFF_Period;
+uint8_t ONperiod, OFFperiod;
 float pTerm, iTerm, dTerm;
-int error;
-int previousError;
+uint8_t error;
+uint8_t previousError;
 
 float kp = 11; //11
 float ki = 0;
 float kd = 11; //11
 
 float output;
-int integral, derivative;
+uint8_t integral, derivative;
 
 sbit irSensor1 = P2^5;  // IR sensor pins
 sbit irSensor2 = P2^6;
@@ -24,16 +27,19 @@ sbit motor2Forward = P1^3;
 sbit motor2Backward = P1^2;
 sbit motor2pwmPin = P3^1;
 
-int motor1newSpeed;
-int motor2newSpeed;
-int motor2Speed = 40; //Default 70
-int motor1Speed = 40; //Default 120
+uint8_t motor1newSpeed;
+uint8_t motor2newSpeed;
+uint8_t motor2Speed = 40; //Default 70
+uint8_t motor1Speed = 40; //Default 120
 
 
-int irReadings[3];
+uint8_t irReadings[3];
 
-void delay(unsigned int time) {
-    unsigned int i, j;
+void setPWM1(uint8_t motor1newSpeed); 
+void setPWM2(uint8_t motor2newSpeed); 
+
+void delay(uint8_t time) {
+    uint8_t i, j;
     for (i = 0; i < time; i++)
         for (j = 0; j < 1275; j++);
 }
@@ -78,6 +84,7 @@ void pidCalculations() {
     previousError = error;
 }
 
+
 void changeMotorSpeed() {
     // Change motor speed of both motors accordingly
     motor2newSpeed = motor2Speed + output;
@@ -86,12 +93,54 @@ void changeMotorSpeed() {
     motor2newSpeed = (motor2newSpeed > 255) ? 255 : motor2newSpeed;
     motor1newSpeed = (motor1newSpeed > 255) ? 255 : motor1newSpeed;
     // Set new speed, and run motors in the forward direction
-    motor2pwmPin = motor2newSpeed;
-    motor1pwmPin = motor1newSpeed;
+   
+		setPWM1(motor1newSpeed);
+		setPWM2(motor2newSpeed);
+    
     motor2Forward = 1;
     motor2Backward = 0;
     motor1Forward = 1;
     motor1Backward = 0;
+		
+		
+}
+
+void setPWM1(uint8_t motor1newSpeed) {
+		period = 65535 - 256;  // FFFF - period
+		ONperiod = 65535 - motor1newSpeed ;
+		OFFperiod = period + ONperiod ; // 65535 - 256(period) + ontime
+}
+
+void setPWM2(uint8_t motor2newSpeed) {
+		period = 65535 - 256;  // FFFF - period
+		ONperiod = 65535 - motor2newSpeed ;
+		OFFperiod = period + ONperiod ; // 65535 - 256(period) + ontime
+}
+
+void setTimer() {
+		TMOD = 0x01;
+		if(motor1newSpeed > 1)
+		{
+			TH0 = (ON_Period >> 8);
+			TL0 = ON_Period;
+		}	
+		else
+		{
+			TH0 = (OFF_Period >> 8);
+			TL0 = OFF_Period;
+		}	
+	
+		if(motor2newSpeed > 1)
+		{
+			TH0 = (ON_Period >> 8);
+			TL0 = ON_Period;
+		}	
+		else
+		{
+			TH0 = (OFF_Period >> 8);
+			TL0 = OFF_Period;
+		}	
+
 }
 
 void main() {
